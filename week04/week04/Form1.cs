c752ed3aeaa2c9cab6c41d8036ef1635574fac6f
azqkad <azqkad@SZA0030900>
2023-03-14 09:32:19 +0100

    utolsó

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -25,6 +25,7 @@ namespace week04
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         void LoadData()
@@ -42,14 +43,14 @@ namespace week04
                 xlWB = xlApp.Workbooks.Add(Missing.Value); //missing value miatt van a reflection a usingnál
                 xlSheet = xlWB.ActiveSheet;
 
-                //CreateTable();
+                CreateTable();
 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
             }
             catch (Exception ex)
             {
-                string ErrorMsg= string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                string ErrorMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
                 MessageBox.Show(ErrorMsg, "Error");
 
                 xlWB.Close(false, Type.Missing, Type.Missing);
@@ -60,6 +61,70 @@ namespace week04
             }
 
 
+        }
+
+        void CreateTable() //private nem feltétlenül kell
+        {
+            string[] headers = new string[] {  //fejléc
+     "Kód",
+     "Eladó",
+     "Oldal",
+     "Kerület",
+     "Lift",
+     "Szobák száma",
+     "Alapterület (m2)",
+     "Ár (mFt)",
+     "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)  // tömb első eleme 0, ezért kell i+1
+            {
+                xlSheet.Cells[/*sor*/1, /*oszlop*/i+1] = headers[i];
+            }
+
+            object[,] values = new object[_flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in _flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator)
+                    values[counter, 4] = "Igen";
+                else
+                    values[counter, 4] = "Nem";
+
+                //módosítani kell, hogy igen/nem jelenjen meg
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "="+GetCell(counter+2, 8)+"*1000000"+"/"+ GetCell(counter + 2, 7); //Excelben I oszlop
+                counter++;
+            }
+
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            //xlSheet.get_Range(GetCell(2, 1), values.GetLength(0), values.GetLength(1)).Value2
+
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }
