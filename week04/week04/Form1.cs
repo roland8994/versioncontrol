75f87089cd6553435972ab3537b1c71a1347933a
azqkad <azqkad@SZA0030900>
2023-03-14 09:02:46 +0100

    6

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -16,6 +16,11 @@ namespace week04
     {
         List<Flat> _flats;
         RealEstateEntities context = new RealEstateEntities();
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +30,36 @@ namespace week04
         void LoadData()
         {
             _flats = context.Flats.ToList();
+        }
+
+        void CreateExcel()
+        {
+            try
+            {
+
+
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value); //missing value miatt van a reflection a usingnál
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string ErrorMsg= string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(ErrorMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+
+            }
+
+
         }
     }
 }
